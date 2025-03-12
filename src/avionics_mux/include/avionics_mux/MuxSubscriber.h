70c44142856372b8b44cc2521f9eb768f77f0180
Eliot Abramo <eliot.abramo@epfl.ch>
2025-03-12 23:12:44 +0100

    subscriber build fixes

--- src/avionics_mux/include/avionics_mux/MuxSubscriber.h
+++ src/avionics_mux/include/avionics_mux/MuxSubscriber.h
@@ -7,21 +7,25 @@
 #ifndef MUX_SUBSCRIBER_H
 #define MUX_SUBSCRIBER_H
 
+#include <string>
 #include "rclcpp/rclcpp.hpp"
+
 #include "BRoCo/CANBus.h"
+#include "Protocol/Protocol.h"
+
 #include "custom_msg/msg/mass_array.hpp"
+#include "custom_msg/msg/mass_calib_offset.hpp"
 
 class MuxSubscriber {
 public:
-    MuxSubscriber(
-        rclcpp::Node* parent,
-        const std::string& topic_name,
-        );
+    MuxSubscriber(CANBus* bus, rclcpp::Node* parent);
 
 private:
 
     rclcpp::Node* parent;
     CANBus* bus;
+    
+    void MuxSubscriber::massDrillCalibOffsetCallback(const custom_msg::msg::MassCalibOffset::SharedPtr msg);
 
     // Declare a pointer which will be used in the cpp file to create a sub instance
     //rclcpp::Subscription<custom_msg::msg::LedsCommand>::SharedPtr led_req_sub;
--- src/avionics_mux/src/MuxSubscriber.cpp
+++ src/avionics_mux/src/MuxSubscriber.cpp
@@ -1,35 +1,33 @@
-// /*
-//  * MuxSubscriber.cpp
-//  *
-//  *      Author: Matas Jones
-//  */
+/*
+ * MuxSubscriber.cpp
+ *
+ *      Author: Matas Jones, Eliot Abramo
+ */
 
-// #include "MuxSubscriber.h"
+ #include <chrono>
+#include <memory>
+#include <functional>
+#include "rclcpp/rclcpp.hpp"
 
-// #include "MuxManager.h"
-// #include "BRoCo/CanSocketDriver.h"
-// #include "Utils.h"
+#include "MuxSubscriber.h"
+#include "MuxManager.h"
 
-// #include "rclcpp/rclcpp.hpp"
+#include "BRoCo/CanSocketDriver.h"
 
-// #include <chrono>
-// #include <memory>
-// #include <functional>
+using namespace std::chrono_literals;
+using std::placeholders::_1;
 
-// using namespace std::chrono_literals;
-// using std::placeholders::_1;
+// Create subscribers based on the pointers in h file and attach callbacks
+MuxSubscriber::MuxSubscriber(CANBus* bus, rclcpp::Node* parent) : bus(bus), parent(parent) {
 
-// // Create subscribers based on the pointers in h file and attach callbacks
-// MuxSubscriber::MuxSubscriber(CANBus* bus, rclcpp::Node* parent) : bus(bus), parent(parent) {
-
-//     RCLCPP_INFO(parent->get_logger(), "Creating subscribers");
+    RCLCPP_INFO(parent->get_logger(), "Creating subscribers");
     
-//     // This block creates a subscriber on a specified topic and binds it to a callback
-//     this->led_req_sub = parent->create_subscription<custom_msg::msg::LedsCommand>
-//         (get_prefix() + get_param<std::string>("LED_REQ_TOPIC"), 10, std::bind(&MuxSubscriber::ledReqCallback, this, _1));
+    // This block creates a subscriber on a specified topic and binds it to a callback
+    this->led_req_sub = parent->create_subscription<custom_msg::msg::MassArray>
+        (get_prefix() + get_param<std::string>("MASS_REQ_ARRAY"), 10, std::bind(&MuxSubscriber::massDrillCalibOffsetCallback, this, _1));
 
-//     RCLCPP_INFO(parent->get_logger(), "Subscribers created");
-// }
+    RCLCPP_INFO(parent->get_logger(), "Subscribers created");
+}
 
 // void MuxSubscriber::ledReqCallback(const custom_msg::msg::LedsCommand::SharedPtr msg) {
 //     uint16_t id = 0;
@@ -45,5 +43,25 @@
 //     set_destination_id(id);
 //     bus->send(&packet);
 // }
+
+void MuxSubscriber::massDrillCalibOffsetCallback(const custom_msg::msg::MassCalibOffset::SharedPtr msg) {
+    uint16_t id = 0;
+    if (msg->destination_id != 0)
+        id = msg->destination_id;
+    else
+        id = get_node_id("SC_DRILL_NODE_ID");
+
+    RCLCPP_INFO(parent->get_logger(), "Sending offset calibration request to node ID %s...", std::to_string(id).c_str());
+    static MassCalibPacket packet;
+    packet.calib_offset = true;
+    packet.calib_scale = false;
+    packet.channel = msg->channel;
+    packet.expected_weight = 1;
+
+    MAKE_IDENTIFIABLE(packet);
+    MAKE_RELIABLE(packet);
+    set_destination_id(id);
+    bus->send(&packet);
+}
 
 
